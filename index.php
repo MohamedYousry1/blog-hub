5cd41c164c7962f0eafb0176d7cd398547914a33
MohamedYousry1 <mohamedyosry701@gmail.com>
2026-05-21 22:00:34 +0300

    display single post in view post

--- index.php
+++ index.php
@@ -1,53 +1,52 @@
-
 <?php require_once 'inc/header.php';
 require_once 'config/connection.php';
 ?>
-<?php 
+<?php
 $query = "SELECT * FROM posts";
 $result = mysqli_query($connection, $query);
 $fetch_posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
-    <link rel="stylesheet" href="assets/css/index.css">
-    <!-- Page Content -->
-    <!-- Banner Starts Here -->
-    <div class="banner header-text">
-      <div class="owl-banner owl-carousel">
-        <div class="banner-item-01">
-          <div class="text-content">
-            <h4>Best Offer</h4>
-            <h2>New Arrivals On Sale</h2>
-          </div>
-        </div>
-        <div class="banner-item-02">
-          <div class="text-content">
-            <h4>Flash Deals</h4>
-            <h2>Get your best products</h2>
-          </div>
-        </div>
-        <div class="banner-item-03">
-          <div class="text-content">
-            <h4>Last Minute</h4>
-            <h2>Grab last minute deals</h2>
-          </div>
-        </div>
+<link rel="stylesheet" href="assets/css/index.css">
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="banner header-text">
+  <div class="owl-banner owl-carousel">
+    <div class="banner-item-01">
+      <div class="text-content">
+        <h4>Best Offer</h4>
+        <h2>New Arrivals On Sale</h2>
       </div>
     </div>
-    <!-- Banner Ends Here -->
+    <div class="banner-item-02">
+      <div class="text-content">
+        <h4>Flash Deals</h4>
+        <h2>Get your best products</h2>
+      </div>
+    </div>
+    <div class="banner-item-03">
+      <div class="text-content">
+        <h4>Last Minute</h4>
+        <h2>Grab last minute deals</h2>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
 
-    <div class="latest-products">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <h2>Latest Posts</h2>
-            </div>
-          </div>
+<div class="latest-products">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="section-heading">
+          <h2>Latest Posts</h2>
+        </div>
+      </div>
 
-          <?php if($fetch_posts):
-          foreach($fetch_posts as $post): ?>
+      <?php if ($fetch_posts):
+        foreach ($fetch_posts as $post): ?>
           <div class="col-md-6 col-lg-4 mb-4">
             <article class="post-card card border-0 h-100">
-              <a href="viewPost.php" class="card-img-wrap">
+              <a href="viewPost.php?id=<?= $post['id']; ?>" class="card-img-wrap">
                 <img src="assets/images/postImage/<?= $post['image'] ?>" alt="Blog post cover">
               </a>
               <div class="card-body d-flex flex-column">
@@ -60,28 +59,28 @@ $fetch_posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
                 </h3>
                 <p class="card-text flex-grow-1"><?= $post['body'] ?></p>
                 <div class="d-flex justify-content-end mt-3">
-                  <a href="viewPost.php" class="btn-future">View<i class="fa fa-long-arrow-right"></i></a>
+                  <a href="viewPost.php?id=<?= $post['id']; ?>" class="btn-future">View<i class="fa fa-long-arrow-right"></i></a>
                 </div>
               </div>
             </article>
           </div>
-          <?php endforeach;
-          else: ?>
-            <div class="col-md-12">
-              <div class="no-posts">
-                <div class="no-posts-icon">
-                  <i class="fa fa-file-text-o"></i>
-                </div>
-                <h3>No posts yet</h3>
-                <p>There are no blog posts to show right now. Be the first to share something.</p>
-                <a href="addPost.php" class="filled-button">Add New Post</a>
-              </div>
+        <?php endforeach;
+      else: ?>
+        <div class="col-md-12">
+          <div class="no-posts">
+            <div class="no-posts-icon">
+              <i class="fa fa-file-text-o"></i>
             </div>
-          <?php endif; ?>
-
-
+            <h3>No posts yet</h3>
+            <p>There are no blog posts to show right now. Be the first to share something.</p>
+            <a href="addPost.php" class="filled-button">Add New Post</a>
+          </div>
         </div>
-      </div>
+      <?php endif; ?>
+
+
     </div>
-    
+  </div>
+</div>
+
 <?php require_once 'inc/footer.php' ?>
--- viewPost.php
+++ viewPost.php
@@ -1,4 +1,14 @@
-<?php require_once 'inc/header.php' ?>
+<?php require_once 'inc/header.php';
+require_once 'config/connection.php';
+
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+}
+$query = "SELECT * FROM posts where id = '$id';";
+$result = mysqli_query($connection, $query);
+$fetch_posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
 <link rel="stylesheet" href="assets/css/viewPost.css">
 
 <div class="page-heading products-heading header-text">
@@ -22,28 +32,33 @@
           <h2>Post Details</h2>
         </div>
       </div>
-      <div class="col-md-6">
-        <div class="right-image">
-          <img src="assets/images/feature-image.jpg" alt="Post cover">
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="left-content">
-          <div class="post-detail__meta">
-            <span class="post-date"><i class="fa fa-calendar"></i> 2026-05-12</span>
-            <span class="tag-chip">Blog</span>
+      <?php if ($fetch_posts):
+        foreach ($fetch_posts as $post): ?>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/postImage/<?= $post['image'] ?>" alt="Post cover">
+            </div>
           </div>
-          <h4>Who we are &amp; What we do?</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat. Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis.</p>
-          <div class="post-detail__actions">
-            <a href="index.php" class="btn-back">
-              <i class="fa fa-long-arrow-left"></i> Back to posts
-            </a>
-            <a href="editPost.php" class="btn btn-outline-primary">Edit post</a>
-            <a href="deletePost.php" class="btn btn-danger">Delete post</a>
+          <div class="col-md-6">
+            <div class="left-content">
+              <div class="post-detail__meta">
+                <span class="post-date"><i class="fa fa-calendar"></i> <?= $post['created_at']; ?></span>
+                <span class="tag-chip">Blog</span>
+              </div>
+              <h4><?= $post['title']; ?></h4>
+              <p><?= $post['body']; ?></p>
+              <div class="post-detail__actions">
+                <a href="index.php" class="btn-back">
+                  <i class="fa fa-long-arrow-left"></i> Back to posts
+                </a>
+                <a href="editPost.php" class="btn btn-outline-primary">Edit post</a>
+                <a href="deletePost.php" class="btn btn-danger">Delete post</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+      <?php
+        endforeach;
+      endif; ?>
     </div>
   </div>
 </div>
