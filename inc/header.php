8f507b18ae7baf2e4785343a7e42fa1b7529f364
MohamedYousry1 <mohamedyosry701@gmail.com>
2026-06-04 19:35:49 +0300

    using lang folder for localization

--- inc/header.php
+++ inc/header.php
@@ -1,19 +1,32 @@
+<?php session_start();
+if (isset($_SESSION['lang'])) {
+  $lang = $_SESSION['lang'];
+} else {
+  $lang = 'en';
+}
+
+if ($lang == 'ar') {
+  require_once 'lang/lang_ar.php';
+} else {
+  require_once 'lang/lang_en.php';
+}
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?= $lang ?>" dir="<?= $language['Dir'] ?>">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <title>Blog</title>
+  <title>Blog</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!--
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"> <!--
 
     TemplateMo 546 Sixteen Clothing
 
@@ -21,54 +34,53 @@
 
     -->
 
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+  <link rel="stylesheet" href="assets/css/header.css">
 
-  </head>
+</head>
 
 <body>
 
-    <!-- ***** Preloader Start ***** -->
-    <div id="preloader" >
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
+  <!-- Header -->
+  <header class="padding-0">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid px-4">
+        <a class="navbar-brand navbar-brand--with-logo" href="index.php">
+          <span class="navbar-logo-slot" aria-hidden="true">
+            <!-- <img src="assets/images/logo.png" alt=""> -->
+          </span>
+          <h2><em><?= $language['Blog'] ?></em></h2>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php"><?= $language['AllPosts'] ?>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="addPost.php"><?= $language['AddNewPost'] ?></a>
+            </li>
+            <?php if (isset($_SESSION['lang']) && $_SESSION['lang'] == 'ar') {
+            ?>
+              <li class="nav-item">
+                <a class="nav-link" href="config/lang.php?lang=en">English</a>
+              </li>
+            <?php } else { ?>
+              <li class="nav-item">
+                <a class="nav-link" href="config/lang.php?lang=ar">العربية</a>
+              </li>
+            <?php } ?>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><?= $language['Logout'] ?></a>
+            </li>
+          </ul>
         </div>
-    </div>  
-    <!-- ***** Preloader End ***** -->
-
-    <!-- Header -->
-    <header class="padding-0">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand" href="index.php"><h2> <em>Blog</em></h2></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="index.php">All Posts
-                  <span class="sr-only">(current)</span>
-                </a>
-              </li> 
-              <li class="nav-item">
-                <a class="nav-link" href="addPost.php">Add New Post</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">English</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">العربية</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
+  </header>
